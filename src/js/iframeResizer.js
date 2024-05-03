084a8b30b470e719e4e54e1a79020bd2291243a4
damlaulku <damla@videolity.com>
2024-05-03 14:35:39 +0300

    eng yorum satırı

--- src/js/iframeResizer.js
+++ src/js/iframeResizer.js
@@ -1,9 +1,13 @@
+// you can read the video ratio from the viewRatio value in the video URL
+// or you can write it here statically.
 let aspectRatio = 1920 / 950;
 
+// updates the aspectRatio value.
 const setAspectRatio = (newAspectRatio) => {
     aspectRatio = newAspectRatio;
 };
 
+// resizes the iframe when the screen size changes.
 const handleResize = () => {
     const iframe = document.querySelector('iframe');
     const width = iframe.offsetWidth;
@@ -11,6 +15,7 @@ const handleResize = () => {
 };
 
 const handleMessage = (event) => {
+    // checks whether the incoming message is a request to update the video size.
     if (event.data && event.data.for === 'videolityIframeResize') {
         setAspectRatio(event.data.aspectRatio);
         handleResize();
@@ -19,8 +24,10 @@ const handleMessage = (event) => {
 
 document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('message', handleMessage, false);
+    // event listener is added to update the iframe size when the screen size changes.
     window.addEventListener('resize', handleResize);
 
+    // first resizing process is done.
     handleResize();
 
     window.addEventListener('beforeunload', () => {
