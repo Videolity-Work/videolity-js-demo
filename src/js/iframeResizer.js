5b1a8a903726d3d508ccf395200bde5e9e8e353a
damlaulku <damla@videolity.com>
2024-05-03 12:11:21 +0300

    new aspectRatio system

--- src/js/iframeResizer.js
+++ src/js/iframeResizer.js
@@ -1,24 +1,22 @@
-// Başlangıçta belirlenmiş en-boy oranı (16:9)
-let aspectRatio = 16 / 8;
+let aspectRatio = 1920 / 950;
 
 const setAspectRatio = (newAspectRatio) => {
     aspectRatio = newAspectRatio;
 };
 
-const handleMessage = (event) => {
-    if (event.data && event.data.for === 'videolityIframeResize') {
-        setAspectRatio(event.data.aspectRatio);
-    }
-};
-
-// iframe'ın yüksekliğini ayarlayan fonksiyon
 const handleResize = () => {
     const iframe = document.querySelector('iframe');
     const width = iframe.offsetWidth;
     iframe.style.height = `${width / aspectRatio}px`;
 };
 
-// iframe'den gelen iletileri dinle ve pencere boyutu değiştiğinde iframe yüksekliğini güncelle
+const handleMessage = (event) => {
+    if (event.data && event.data.for === 'videolityIframeResize') {
+        setAspectRatio(event.data.aspectRatio);
+        handleResize();
+    }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('message', handleMessage, false);
     window.addEventListener('resize', handleResize);
